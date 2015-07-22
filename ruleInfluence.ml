e1bd79d6f984b378bbc45655f28a47faf2ed75e3
Jordan Thayer <jthayer@draper.com>
2015-07-22 11:51:24 -0400

    cleaning up code a bit

--- ruleInfluence.ml
+++ ruleInfluence.ml
@@ -60,33 +60,33 @@ module VarNodes (M : ArgOffset) = struct
 end
 
 
+(* This looks horrifying, but soldier on.  To get a perfect hash function on the
+   nodes in the graph, we need to make the modules locally, since we don't know
+   how large the rules are going to be until the function gets called, so it
+   all ends up bundled in here. *)
+
 let processRule (rule : Comrule.rule) =
   let (lfName, lhsArgs) = rule.Comrule.lhs
   and cond = rule.Comrule.cond in
   let module AO = struct
     let lhsArgs = List.length lhsArgs
-    let condArgs = 2 * (List.length cond)
-  end in
+    let condArgs = 2 * (List.length cond) end in
   let module Node = VarNodes(AO) in
-  let module RuleGraph =
+  let module G =
         Graph.Imperative.Digraph.ConcreteLabeled(Node)(QualEdge) in
-  let module Reachability = Graph.Fixpoint.Make(RuleGraph)
-        (struct
-          type vertex = RuleGraph.E.vertex
-          type edge = RuleGraph.E.t
-          type g = RuleGraph.t
-          type data = bool
-          let direction = Graph.Fixpoint.Forward
-          let equal = (=)
-          let join = (||)
-          let analyze _ = (fun x -> x)
-         end)  in
-  let graph = RuleGraph.create () in
+  let module Reachability = Graph.Fixpoint.Make(G) (struct
+    type vertex = G.E.vertex
+    type edge = G.E.t
+    type g = G.t
+    type data = bool
+    let direction = Graph.Fixpoint.Forward
+    let equal = (=)
+    let join = (||)
+    let analyze _ = (fun x -> x) end)  in
+  let graph = G.create () in
   let lhsAdd i poly =
-    let toAdd = LHS { var = poly;
-                      pos = i;
-                      qual = Equal } in
-    RuleGraph.add_vertex graph toAdd;
+    let toAdd = LHS { var = poly; pos = i; qual = Equal } in
+    G.add_vertex graph toAdd;
     toAdd in
   let condAdd left atom =
     let right = left + 1
@@ -97,50 +97,31 @@ let processRule (rule : Comrule.rule) =
       | Pc.Gtr (p1, p2)
       | Pc.Leq (p1, p2)
       | Pc.Lss (p1, p2) -> (Delta, p1, p2) in
-    (** We need both the identity nodes for each element of the inequality.
-        Then, we need the nodes that capture the nature of the delta.
-     **)
     let a1 = Cond { var = p1; pos = left; qual = Equal; }
-    and a2 = Cond { var = p2; pos = right; qual = Equal; }
-    and a1' = Cond { var = p1; pos = left; qual = qual; }
-    and a2' = Cond { var = p2; pos = right; qual = qual; } in
+    and a2 = Cond { var = p2; pos = right; qual = Equal; } in
     (** Information really flows both ways across the inequality *)
-    let edge1 = RuleGraph.E.create a1 qual a2'
-    and edge2 = RuleGraph.E.create a2 qual a1' in
-    RuleGraph.add_edge_e graph edge1;
-    RuleGraph.add_edge_e graph edge2;
+    G.E.create a1 qual (withQual qual a2) |> G.add_edge_e graph;
+    G.E.create a2 qual (withQual qual a1) |> G.add_edge_e graph;
     (a1,a2) in
   let addEdge ?(graph = graph) rp1 rp2 =
     if shareVars rp1 rp2 then
-      begin
-        let qual = if Poly.compare (getPoly rp1) (getPoly rp2) = 0 then
-            Equal else
-            Delta in
-        let rp1' = if qual = (getQual rp1) then rp1
-          else withQual qual rp1 in
-        let rp2' =
-          if qual = (getQual rp2) then rp2
-          else withQual qual rp2 in
-        let edge = RuleGraph.E.create rp1 qual rp2'
-        and edge2 = RuleGraph.E.create rp2' qual rp1
-        and edge3 = RuleGraph.E.create rp1' qual rp2
-        and edge4 = RuleGraph.E.create rp2 qual rp1' in
-        RuleGraph.add_edge_e graph edge;
-        RuleGraph.add_edge_e graph edge2;
-        RuleGraph.add_edge_e graph edge3;
-        RuleGraph.add_edge_e graph edge4
-      end in
+      let qual = if Poly.compare (getPoly rp1) (getPoly rp2) = 0 then
+          Equal else
+          Delta in
+        G.E.create rp1 qual (withQual qual rp2) |> G.add_edge_e graph;
+        G.E.create (withQual qual rp2) qual rp1 |> G.add_edge_e graph;
+        G.E.create (withQual qual rp1) qual rp2 |> G.add_edge_e graph;
+        G.E.create rp2 qual (withQual qual rp1) |> G.add_edge_e graph in
   let joinToCond ?(graph = graph) (lhsNode : rulePos) ((c1 : rulePos), (c2 : rulePos)) =
     addEdge lhsNode c1;
     addEdge lhsNode c2  in
   let (lhsNodes : rulePos list) = List.mapi lhsAdd lhsArgs
   and (condNodes : (rulePos * rulePos) list) = List.mapi condAdd cond in
-  List.iter (fun lhsN -> List.iter (joinToCond lhsN) condNodes) lhsNodes;
   let dealWithRHS (rFName, rArgs) =
-    let graph' = RuleGraph.copy(graph) in
-    let rhsNodes = List.mapi (fun i poly -> 
+    let graph' = G.copy(graph) in
+    let rhsNodes = List.mapi (fun i poly ->
       let toAdd = RHS { var = poly; pos = i; qual = Equal } in
-      RuleGraph.add_vertex graph' toAdd;
+      G.add_vertex graph' toAdd;
       toAdd) rArgs in
     (* Stitch the right hand side nodes together *)
     List.iter (fun rn ->
@@ -160,7 +141,9 @@ let processRule (rule : Comrule.rule) =
         else if rtest rn
         then ({lPos = lpos; rPos = rpos; qual = Equal;} :: accum, ri')
         else (accum, ri')) ([], 0) rhsNodes |> fst) lhsNodes in
+  List.iter (fun lhsN -> List.iter (joinToCond lhsN) condNodes) lhsNodes;
   Utils.concatMap dealWithRHS rule.Comrule.rhss
+
 
 let main () =
   let usage = ""
