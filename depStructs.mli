0d6c6862bbf658ffae16a60baf6a3fb3eb322cd4
Jordan Thayer <jthayer@draper.com>
2015-07-27 09:05:35 -0400

    comments

--- depStructs.mli
+++ depStructs.mli
@@ -59,6 +59,13 @@ val ruleTransToString : ruleTrans -> string
     list *)
 val doVis : ruleTrans list -> string -> unit
 
+(** The two steps of doVis.  Split up so that if you both want to draw the
+    information flow graph and compute which argument positions are reachable
+    from a given set of starts, you can do that more efficiently. *)
 val buildFlowGraph : ruleTrans list -> ArgPosGraph.t
 val draw : ArgPosGraph.t -> string -> unit
+
+(** given an argument position list and a flow graph, return a function which
+    gives true if an argument position could be reached from those starts, and
+    false otherwise. *)
 val reachable : argPos list -> ArgPosGraph.t -> (argPos -> bool)
