6a385f0e864c9798c443baa10452dffff70dcd49
Jordan Thayer <jthayer@draper.com>
2015-09-03 15:50:50 -0400

    Repairing inexplicable git error
    Some mli file was or wasn't added, and then it was out of date.  We made it's signature match cprocs/cmeta, and now it works.

--- cintprocs/cintmeta.mli
+++ cintprocs/cintmeta.mli
@@ -2,4 +2,7 @@ module CTRSObl = Cintfarkaspolo.CTRSObl
 val process :
   CTRSObl.CTRS.RuleT.rule list ->
   int ->
-  Term.funSym -> Complexity.ctype -> (Complexity.t * (unit -> string)) option
+  Term.funSym ->
+  Annot.t ->
+  Complexity.ctype ->
+  (Complexity.t * (unit -> string)) option
