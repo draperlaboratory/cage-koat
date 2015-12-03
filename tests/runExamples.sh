dd09462b1b8a2779fb3eb515ee3e1a7a2c28471e
jthayer@draper.com <jthayer@draper.com>
2015-12-03 09:10:33 -0500

    Moving some more tests around. Also adding a script for running a batch of examples through koat.

---
+++ tests/runExamples.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+## Run KoaT against every instance in the test directory
+
+DEBUG=true
+KOAT=./koat.native
+OUTDIR=foo
+
+
+runDirectory()
+{
+    dir=$1
+    for file in $dir/*.koat; do
+        if [ -e $file ]; then
+            filename=$(basename $file)
+            if $DEBUG; then
+                echo "./koat.native $file > ${file}.out"
+                echo "mv ${file}.out $filename"
+            else
+                $KOAT $file > ${file}.out
+                mv ${file}.out $OUTDIR/${filename}.out
+            fi
+        fi
+    done
+}
+
+legalOutDir()
+{
+    outdir=$1
+    if [ -e $outdir ]; then
+        if [ -d $outdir ]; then
+            for file in $dir/* ; do
+                if [ -e $file ]; then
+                    echo "Output Directory already had files in it. Aborting."
+                    exit 1
+                fi
+            done
+        else
+            echo "Output directory isn't a directory."
+            exit 1
+        fi
+    else
+        echo "Output directory doesn't exist. Creating it."
+        mkdir $outdir
+    fi
+}
+
+legalKoat()
+{
+    if [ -e $KOAT ]; then
+        return
+    else
+        echo "Couldn't find KoAT. Expected it at $KOAT"
+        if $DEBUG; then
+            return
+        else
+            exit 1
+        fi
+    fi
+}
+
+main()
+{
+    legalKoat
+    legalOutDir $OUTDIR
+    for dir in inputs/*; do
+        runDirectory $dir
+    done
+}
+
+main
