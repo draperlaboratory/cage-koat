8887764f51ccb34ea06dd784e51f540257b2b692
mschaper <michael.schaper@uibk.ac.at>
2017-09-12 14:43:23 +0200

    move: apronize.ml already in translation dir

--- translation/apronize.ml
+++ translation/apronize.ml
@@ -3,7 +3,7 @@
 
   Runs the ApronInvariantsProcessor on the original complexity problem and
   renders the new complexity problem again to stdout.
-  In short, this module provides invariant generation for its problems.
+  In short, this module provides invariant generation for ITS problems.
 *)
 
 
@@ -20,7 +20,6 @@ open CTRS
 
 module UnreachableProc = DeleteUnreachableProc.Make(RVG)
 module UnsatProc = DeleteUnsatProc.Make(RVG)
-module SlicingProc = SlicingProc.Make(RVG)
 
 IFDEF HAVE_APRON THEN
 module ApronInvariantsProc = ApronInvariantsProcessor.MakeKoatProc(RVG)
@@ -49,19 +48,12 @@ let apply doit proc ((ctrsobl, tgraph, rvgraph) as prob) =
 
 let (|>) v f = f v ;;
 
-let slicer ((ctrsobl, tgraph, rvgraph) as prob) =
-  match SlicingProc.process ~thresh:1 ctrsobl with
-  | None -> prob
-  | Some (obl', proof) -> (obl', TGraph.compute obl'.ctrs.rules, None)
-
 let apronize ctrsobl simplify =
   let tgraph   = TGraph.compute ctrsobl.ctrs.rules in
   (* let lscs     = LSC.computeLocalSizeComplexities ctrsobl.ctrs.rules  in
   let rvgraph  = Some (RVG.compute lscs tgraph) in *)
   let rvgraph  = None in
-  let prob = (ctrsobl,tgraph,rvgraph) in
-  let prob' = if simplify then slicer prob else prob in
-  prob'
+  (ctrsobl,tgraph,rvgraph)
   |> apply simplify doUnsatRemoval
   |> apply simplify doUnreachableRemoval
   |> apply true     doApronInvariants
@@ -90,17 +82,43 @@ and print_usage () = Arg.usage args usage
 let rec ppKoat ctrs =
   "(GOAL COMPLEXITY)\n"
   ^ Printf.sprintf "(STARTTERM (FUNCTIONSYMBOLS %s))\n" ctrs.startFun
-  ^ Printf.sprintf "(VAR %s)\n"  (String.concat " " (CTRS.getVars ctrs))
-  ^ Printf.sprintf "(RULES\n  %s\n)" (String.concat "\n  " (List.rev_map ppRule ctrs.rules))
+  ^ Printf.sprintf "(VAR %s)\n"  (String.concat " " (List.map Poly.stringOfVar (CTRS.getVars ctrs)))
+  ^ Printf.sprintf "(RULES\n  %s\n)" (String.concat "\n  " (List.map ppRule ctrs.rules))
+and ppTerm { Term.fn = f; Term.args = args } =
+  Printf.sprintf "%s(%s)" f (String.concat ", " (List.map Poly.toStringSimple args))
 and ppRule rule =
   if Poly.equal rule.Rule.lowerBound Poly.one && Poly.equal rule.Rule.upperBound Poly.one
     then Rule.toString rule
     else
-      (Term.toString rule.lhs)
+      (ppTerm rule.lhs)
       ^ Printf.sprintf " -{%s,%s}> "  (Poly.toString rule.lowerBound) (Poly.toString rule.upperBound)
-      ^ Printf.sprintf "Com_%i(%s)" (List.length rule.rhss) (String.concat ", " (List.map Term.toString rule.rhss))
+      ^ Printf.sprintf "Com_%i(%s)" (List.length rule.rhss) (String.concat ", " (List.map ppTerm rule.rhss))
       ^ (if rule.cond = [] then "" else " [ " ^ (Pc.toString rule.cond) ^ " ]")
   ;;
+
+let set_timer tsecs =
+  ignore (Unix.setitimer Unix.ITIMER_REAL { Unix.it_interval = 0.0; Unix.it_value = tsecs })
+
+exception Timeout
+
+let handle_sigalrm signo =
+  raise Timeout
+
+let timed_run4 tsecs f arg1 arg2 =
+  let oldsig = Sys.signal Sys.sigalrm (Sys.Signal_handle handle_sigalrm) in
+    try
+      set_timer tsecs;
+      let res = f arg1 arg2 in
+        set_timer 0.0;
+        Sys.set_signal Sys.sigalrm oldsig;
+        res
+    with
+      | Timeout ->
+        (
+          Sys.set_signal Sys.sigalrm oldsig;
+          Printf.eprintf "[time out] %s" !filename;
+          exit 1
+        )
 
 let main () =
   Arg.parse args (fun f -> filename := f) usage;
@@ -111,9 +129,11 @@ let main () =
         exit 1
       )
     else
-      let (startfun, cint) = Parser.parseCint !filename SimpleT.Ctrls in
-      let ctrsobl  = getInitialObl cint startfun Complexity.Time in
-      let nctrsobl = apronize ctrsobl !simplify in
+      let (startfun, cint) = Its_aux.parse !filename in
+      let ctrsobl  = getInitialObl cint startfun in
+      let nctrsobl = timed_run4 30.0
+        apronize ctrsobl !simplify
+      in
       Printf.printf "%s" (ppKoat nctrsobl.ctrs);
       exit 0
   ;;
